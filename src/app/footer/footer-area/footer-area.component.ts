3dc7eef628e5d98aa8695cfdac55f58942aaae47
Dennis Ammen <dennis.ammen@outlook.com>
2023-03-23 00:17:22 +0100

    Added footer area component

---
+++ src/app/footer/footer-area/footer-area.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-footer-area',
+  templateUrl: './footer-area.component.html',
+  styleUrls: ['./footer-area.component.scss']
+})
+export class FooterAreaComponent {
+
+}
