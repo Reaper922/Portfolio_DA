81be3950edbdc3a0ff0c0c6bf227d3f99b560176
Dennis Ammen <dennis.ammen@outlook.com>
2023-03-23 00:20:16 +0100

    Angular adjustments and module/component imports

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FooterAreaComponent } from './footer/footer-area/footer-area.component';
+import { LogoComponent } from './logo/logo.component';
+import { SocialMediaComponent } from './footer/social-media/social-media.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FooterAreaComponent,
+    LogoComponent,
+    SocialMediaComponent
   ],
   imports: [
     BrowserModule,
