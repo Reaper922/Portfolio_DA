23c09fd8fda35f0c1bcccab051b86245a54c8cb6
Dennis Ammen <dennis.ammen@outlook.com>
2023-03-25 22:09:17 +0100

    Angular module imports

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,25 @@ import { AppComponent } from './app.component';
 import { FooterAreaComponent } from './footer/footer-area/footer-area.component';
 import { LogoComponent } from './logo/logo.component';
 import { SocialMediaComponent } from './footer/social-media/social-media.component';
+import { SkillsetAreaComponent } from './skillset/skillset-area/skillset-area.component';
+import { SkillGridComponent } from './skillset/skill-grid/skill-grid.component';
+import { IntroductionAreaComponent } from './introduction/introduction-area/introduction-area.component';
+import { ButtonComponent } from './button/button.component';
+import { ProjectsAreaComponent } from './projects/projects-area/projects-area.component';
+import { ProjectComponent } from './projects/project/project.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     FooterAreaComponent,
     LogoComponent,
-    SocialMediaComponent
+    SocialMediaComponent,
+    SkillsetAreaComponent,
+    SkillGridComponent,
+    IntroductionAreaComponent,
+    ButtonComponent,
+    ProjectsAreaComponent,
+    ProjectComponent,
   ],
   imports: [
     BrowserModule,
