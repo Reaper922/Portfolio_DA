76787a334d2044b6939f3b81fe36f76ee5150141
Dennis Ammen <dennis.ammen@outlook.com>
2023-03-25 16:44:46 +0100

    Added skill grid component

---
+++ src/app/skillset/skill-grid/skill-grid.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-skill-grid',
+  templateUrl: './skill-grid.component.html',
+  styleUrls: ['./skill-grid.component.scss']
+})
+export class SkillGridComponent {
+
+}
