b345069133685925cca7beb8ddce65ac25631000
Dennis Ammen <dennis.ammen@outlook.com>
2023-04-06 23:48:51 +0200

    Adjusted projects and descriptions

--- src/app/projects/projects-area/projects-area.component.ts
+++ src/app/projects/projects-area/projects-area.component.ts
@@ -10,38 +10,35 @@ export class ProjectsAreaComponent {
   projects: project[] = [
     {
       title: "Join",
-      stack: "Angular | TypeScript | HTML | CSS | Firebase",
-      description: "Task manager inspired by the Kanban System. Create and organize tasks using drag and drop functions, assign users and categories.",
-      github: "www.github.com",
+      stack: "JavaScript | HTML | CSS",
+      description: "A task manager inspired by the Kanban method. You can create and organize tasks using drag and drop, assign tasks to users and define subtasks.",
+      github: "https://github.com/Reaper922/Join_DA",
+      projectUrl: 'https://join.dennisammen.de',
       prevImageUrl: "/assets/img/join.jpg"
     },
     {
-      title: "El Pollo Loco",
+      title: "Captain Clown Nose",
       stack: "JavaScript | HTML | CSS",
-      description: "Jump, run and throw game based on object-oriented approach. Help Pepe to find coins and tabasco salsa to fight against the crazy hen.",
-      github: "www.github.com",
+      description: "A pirate themed jump and run game implemented using the object-oriented approach. Try your luck and collect all coins! ",
+      github: "https://github.com/Reaper922/PirateGame_DA",
+      projectUrl: 'https://game.dennisammen.de',
       prevImageUrl: "/assets/img/el_pollo_loco.jpg"
     },
     {
       title: "Simple CRM",
       stack: "Angular | TypeScript | HTML | CSS | Firebase",
       description: "A very Simple Customer Relationship Management system working with CRUD functionality. ",
-      github: "www.github.com",
+      github: "https://www.github.com",
+      projectUrl: '',
       prevImageUrl: "/assets/img/simple_crm.jpg"
     },
     {
       title: "Pokedex",
       stack: "JavaScript | HTML | CSS | API",
       description: "Based on the PokéAPI a simple library that provides and catalogues pokemon information.",
-      github: "www.github.com",
+      github: "https://www.github.com",
+      projectUrl: '',
       prevImageUrl: "/assets/img/pokedex.jpg"
-    },
-    {
-      title: "Crypto Converter",
-      stack: "Angular | TypeScript | Rest-API | HTML | CSS",
-      description: "Based on the PokéAPI a simple library that provides and catalogues pokemon information.",
-      github: "www.github.com",
-      prevImageUrl: "/assets/img/crypto_converter.jpg"
     }
   ]
 }
