fbfe607c8f49b5298d20d831553afd088cb9eb0b
Dennis Ammen <dennis.ammen@outlook.com>
2023-04-06 23:47:49 +0200

    Added main and legal notice routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { MainComponent } from './pages/main/main.component';
+import { LegalNoticeComponent } from './pages/legal-notice/legal-notice.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: MainComponent},
+  {path: 'legal-notice', component: LegalNoticeComponent}
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, {
+    anchorScrolling: 'enabled',
+    scrollPositionRestoration: 'top'
+  })],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
