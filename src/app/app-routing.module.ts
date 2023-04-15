fb4eb4229d6f17d71b53dceb27846fa339c4e8ab
Dennis Ammen <dennis.ammen@outlook.com>
2023-04-15 22:18:31 +0200

    Added privacy policy route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { MainComponent } from './pages/main/main.component';
 import { LegalNoticeComponent } from './pages/legal-notice/legal-notice.component';
+import { PrivacyPolicyComponent } from './pages/privacy-policy/privacy-policy.component';
 
 const routes: Routes = [
   {path: '', component: MainComponent},
-  {path: 'legal-notice', component: LegalNoticeComponent}
+  {path: 'legal-notice', component: LegalNoticeComponent},
+  {path: 'privacy-policy', component: PrivacyPolicyComponent}
 ];
 
 @NgModule({
